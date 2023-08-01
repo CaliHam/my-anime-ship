d2d8668594ef5d91c844dc93927c7973fc431f33
Calli Herrmann <calliherrmann@gmail.com>
2023-08-01 16:27:54 -0400

    Add apicall for fetching zodiac sign for user

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,10 +7,22 @@ const getAllCharacters = async () => {
     return data
 }
 
+const fetchZodiacSign = async (month, day) => {
+    const response = await fetch(`https://my-anime-ship-api.onrender.com/api/v1/zodiac`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(month, day)
+    })
+    if (!response.ok) {
+        throw new Error(response.statusText)
+    }
+    const data = await response.json()
+    return data
+}
 
 
 
 export {
     getAllCharacters,
-
+    fetchZodiacSign
   }
