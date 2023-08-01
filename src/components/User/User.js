2adaacb1db3d755de125513cbeac53a8a551af02
Calli Herrmann <calliherrmann@gmail.com>
2023-08-01 12:23:34 -0400

    Add icons to user page

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom'
 import './User.css'
+import pencil from './pencil.png'
+import next from './right-arrow.png'
 
 const User = ({user, setUser}) => {
-
 
     const handleUserChange = (e) => {
         const {name, value} = e.target
@@ -16,7 +17,7 @@ const User = ({user, setUser}) => {
             <aside className='icon-container'>
                 <img src='https://pbs.twimg.com/media/E3_1i33VkAAI1ub.jpg' alt='user icon' className='user-icon'/>
                 <div class="image-overlay">
-                    <p>Edit</p>
+                    <p><img className='pencil-icon' src={pencil} alt='pencil'/> EDIT</p>
                 </div>
             </aside>
             <form className='user-form'>
@@ -24,9 +25,9 @@ const User = ({user, setUser}) => {
                 <input name='name' value={user.name} onChange={e => handleUserChange(e)}/>
                 <p>Birthday:</p>
                 <input name='birthday' type='date' value={user.birthday} onChange={e => handleUserChange(e)}/>
-                <Link to='/pickcharacter'></Link>
             </form>
         </div>
+        <Link to='/pickcharacter'><img className='next-arrow' src={next} alt='next arrow'/></Link>
     </div>
   )
 }
