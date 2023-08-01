3fdab1396a57398e636a507819ac2a49f3cb79b6
Calli Herrmann <calliherrmann@gmail.com>
2023-08-01 17:29:28 -0400

    Add post request for synastry report

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,29 +1,42 @@
 const getAllCharacters = async () => {
-    const response = await fetch('https://my-anime-ship-api.onrender.com/api/v1/characters')
-    if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-      const data = await response.json()
-    return data
+  const response = await fetch('https://my-anime-ship-api.onrender.com/api/v1/characters')
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+    const data = await response.json()
+  return data
 }
 
 const fetchZodiacSign = async (month, day) => {
-    const response = await fetch(`https://my-anime-ship-api.onrender.com/api/v1/zodiac`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({month, day})
-    })
-    if (!response.ok) {
-        throw new Error(response.statusText)
-    }
-    const sign = await response.json()
-    
-    return sign
+  const response = await fetch(`https://my-anime-ship-api.onrender.com/api/v1/zodiac`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({month, day})
+  })
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  const sign = await response.json()
+  return sign
+}
+
+const postSynastry = async (month1, day1, month2, day2) => {
+  const response = await fetch(`https://my-anime-ship-api.onrender.com/api/v1/synastry`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({month1, day1, month2, day2})
+  })
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+  const report = await response.json()
+  return report
 }
 
 
 
 export {
     getAllCharacters,
-    fetchZodiacSign
+    fetchZodiacSign,
+    postSynastry
   }
