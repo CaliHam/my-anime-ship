0dba48b35227dc8991e3b69876a9930719a94d30
Calli Herrmann <calliherrmann@gmail.com>
2023-08-02 00:10:55 -0400

    Add component for user icons and folder of icon urls

---
+++ src/components/User/UserIcon/icons.js
@@ -0,0 +1,15 @@
+const icons = [
+    'https://u.cubeupload.com/User713646/918Screenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/b64Screenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/35dScreenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/4a3Screenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/85Screenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/2cScreenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/1fbScreenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/131Screenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/9f6Screenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/ac4Screenshot20230801at.png',
+    'https://u.cubeupload.com/User713646/6d5Screenshot20230801at.png'
+]
+
+export default icons;
