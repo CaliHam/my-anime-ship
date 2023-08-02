894ade53bd7a75d19ffddab5225c2a95d20559cf
Calli Herrmann <calliherrmann@gmail.com>
2023-08-02 12:01:35 -0400

    Add icon for an even 12 and add close icon on modal

--- src/components/User/Modal/Modal.js
+++ src/components/User/Modal/Modal.js
@@ -1,11 +1,12 @@
 import './Modal.css'
+import close from './close.png'
 
 const Modal = ({ closeModal, children }) => {
 
   return (
     <div className="modal">
       <div className="modal-content">
-        <button className="modal-close" onClick={closeModal}>Close</button>
+        <button className="modal-close" onClick={closeModal}><img src={close} alt='close modal'/></button>
         {children}
       </div>
     </div>
