e478a2e83850d731698b30d6e47654ff36f186e4
Calli Herrmann <calliherrmann@gmail.com>
2023-08-03 15:30:54 -0400

    Add testing for navbar

---
+++ cypress/e2e/navbar_spec.cy.js
@@ -0,0 +1,22 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/characters', {
+      statusCode: 200,
+      fixture: 'allCharacters',
+    }).as('getAllCharacters')
+    cy.intercept('GET', 'http://localhost:3001/api/v1/savedreports', {
+      statusCode: 200,
+      fixture: 'allSavedReports',
+    }).as('getAllSaved')
+  })
+  it('Should navigate to expected pages', () => {
+    cy.visit('http://localhost:3000')
+    cy.wait('@getAllCharacters')
+    cy.get('.saved-link').contains('Saved Reports').click()
+    cy.wait('@getAllSaved')
+    cy.location('pathname').should('eq', '/savedreports')
+    cy.get('.nav-logo').click()
+    cy.wait('@getAllCharacters')
+    cy.location('pathname').should('eq', '/')
+  })
+})
