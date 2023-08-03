06acff378692f2cd3bb4614f3834e9ab9aaac93e
Calli Herrmann <calliherrmann@gmail.com>
2023-08-02 22:16:02 -0400

    Add nav bar styling to navigate site easier

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom'
 import './Navbar.css'
+import logo from './MyAnimeShip-small.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='nav-bar'>
+      <Link to="/match"><img className='nav-logo' src={logo} alt='My Anime Ship logo'/></Link>
+      <Link to="/savedreports" className='saved-link'>Saved Reports</Link>
+    </nav>
   )
 }
 
