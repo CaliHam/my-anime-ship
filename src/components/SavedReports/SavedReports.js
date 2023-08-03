7aa0b2a259429e94b314b11f36e5ef1b1817f33a
Calli Herrmann <calliherrmann@gmail.com>
2023-08-03 13:58:30 -0400

    Add delete image to saved cards

--- src/components/SavedReports/SavedReports.js
+++ src/components/SavedReports/SavedReports.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import './SavedReports.css'
 import { getSavedReports } from '../../apiCalls'
 import { Link } from 'react-router-dom'
+import x from './x.png'
 
 const SavedReports = ({savedReports, setSavedReports}) => {
 
@@ -14,6 +15,7 @@ const SavedReports = ({savedReports, setSavedReports}) => {
 		return savedReports.map(report => {
 			return (
 				<div key={report.id} id={report.id} className='saved-report'>
+					<img src={x} alt='delete report' className='delete-btn'/>
 					<p>{report.user.name} and {report.selectedMan.name}</p>
 					<p className='saved-report-score'>{report.report.compatibilityScore}</p>
 				</div>
@@ -25,7 +27,7 @@ const SavedReports = ({savedReports, setSavedReports}) => {
 		<div className='saved-report-wrapper'>
 			<h1>Saved Reports</h1>
 			<section className='saved-report-container'>
-				{!savedReports.length ? <p>Loading...</p> : renderReports()}
+				{!savedReports.length ? <p>Make a calculation and save the report to view it here!</p> : renderReports()}
 			</section>
 			<Link to='/match'><button className='classic-button'>Make Another Calculation</button></Link>
 		</div>
